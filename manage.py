c177202ab450cc1ca5e729145851f1875cbc2163
Matty <matthew.j.percival@gmail.com>
2022-12-22 11:53:01 +0000

    setup flask blueprints and tested

--- manage.py
+++ manage.py
@@ -1,7 +1,12 @@
-from flask.cli import FlaskGroup
-from src import app, db
+import sys
 
-cli = FlaskGroup(app)
+from flask.cli import FlaskGroup
+
+from src import create_app, db
+from src.api.models import User
+
+app = create_app()
+cli = FlaskGroup(create_app=create_app)
 
 @cli.command('recreate_db')
 def recreate_db():
--- src/__init__.py
+++ src/__init__.py
@@ -1,38 +1,66 @@
 import os
 
-from flask import Flask, jsonify
-from flask_restx import Resource, Api
+from flask import Flask
+# from flask import Flask, jsonify
+# from flask_restx import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 
-#instantiate the app
-app = Flask (__name__)
-api = Api(app)
+# #instantiate the app
+# app = Flask (__name__)
+# api = Api(app)
 
-#set config
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
-# app.config.from_object('src.config.DevelopmentConfig') - old
+# #set config
+# app_settings = os.getenv('APP_SETTINGS')
+# app.config.from_object(app_settings)
+# # app.config.from_object('src.config.DevelopmentConfig') - old
 
 #instantiate the db
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
-#model
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
+# #model
+# class User(db.Model):
+#     __tablename__ = 'users'
+#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#     username = db.Column(db.String(128), nullable=False)
+#     email = db.Column(db.String(128), nullable=False)
+#     active = db.Column(db.Boolean(), default=True, nullable=False)
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+#     def __init__(self, username, email):
+#         self.username = username
+#         self.email = email
 
-class Ping(Resource):
-    def get(self):
-        return {
-            'status':'success',
-            'message':'pong!'
-        }
+# class Ping(Resource):
+#     def get(self):
+#         return {
+#             'status':'success',
+#             'message':'pong!'
+#         }
 
-api.add_resource(Ping, '/ping')
+# api.add_resource(Ping, '/ping')
+
+#application factory pattern
+def create_app(script_info=None):
+    
+    #instantiate the app
+    app = Flask(__name__)
+
+    #set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    #set up extensions
+    db.init_app(app)
+
+    #register blueprints
+    from src.api.ping import ping_blueprint
+    app.register_blueprint(ping_blueprint)
+
+    #shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+
+    return app
+
+    ##Take note of the shell_context_processor. This is used to register the app and db to the shell. 
+    # Now we can work with the application context and the database without having to import them directly into the shell
